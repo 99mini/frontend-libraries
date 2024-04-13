5474e9123f3927d2fe33d26938b5e70cdacdc86b
mimi99 <0mini9939@gmail.com>
2024-04-13 17:01:17 +0900

    feat: normal toast

--- src/molecular/Toast/Toast.stories.tsx
+++ src/molecular/Toast/Toast.stories.tsx
@@ -1,9 +1,10 @@
-
 import React from "react";
 
 import Toast, { ToastProps } from "./Toast";
 import styles from "./Toast.scss";
 import classNames from "classnames";
+
+import Button from "@atom/Button";
 
 export default {
   component: Toast,
@@ -12,7 +13,22 @@ export default {
 
 const cx = classNames.bind(styles);
 
-const Template = (args: ToastProps) => <Toast {...args} />;
+const Template = (args: ToastProps) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 20 }}>
+      <div style={{ height: 400, width: "100%", backgroundColor: "plum" }}>{"some contents"}</div>
+
+      <Button onClick={() => setOpen(true)}>toast fire</Button>
+      <Toast {...args} open={open} onClose={() => setOpen(false)} style={{ display: "flex", justifyContent: "space-between" }}>
+        <span>{"children message"}</span>
+        <Button style={{ color: "blue" }} onClick={() => setOpen(false)}>
+          {"확인"}
+        </Button>
+      </Toast>
+    </div>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {
--- src/molecular/Toast/Toast.tsx
+++ src/molecular/Toast/Toast.tsx
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 import classNames from "classnames";
-import './Toast.scss'
+import "./Toast.scss";
 
-export type ToastProps = React.DetailedHTMLProps<
-  React.HTMLAttributes<HTMLDivElement>,
-  HTMLDivElement
-> & {};
+export type ToastProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & ToastType & ToastStateType;
 
-const Toast = ({...props}: ToastProps) => {
+export type ToastType = {
+  message?: string;
+  /**
+   * @description Toast type
+   * 1. info
+   * 2. success
+   * 3. error
+   * 4. warning
+   * 5. default
+   */
+  type?: "info" | "success" | "error" | "warning" | "default";
+  /**
+   * @description How long the toast is visible (ms)
+   */
+  duration?: number;
+};
+
+type ToastStateType = {
+  open: boolean;
+  onClose: () => void;
+};
+
+const Toast = ({ ...props }: ToastProps) => {
+  const { message, type, duration, open, onClose, ...htmlProps } = { ...props };
+
+  const [toast, setToast] = useState<ToastType>({ message: message, type: type ?? "default", duration: duration ?? 3000 });
+  const [isAnimated, setIsAnimated] = useState(true);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    if (open) {
+      setIsAnimated(false);
+      timer = setTimeout(() => {
+        onClose();
+      }, toast.duration);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [open]);
+
   return (
-    <div {...props} className={classNames("Mini-Toast", props.className)}>
-      {props.children ?? "Toast"}
-    </div>
+    <ToastContext.Provider value={{ toast, setToast }}>
+      <div className={classNames("Mini-Toast-root", open ? "open" : isAnimated ? "close" : "fade-out", props.className)}>
+        <div className={classNames("Mini-Toast")} {...htmlProps}>
+          {!props.children && (toast.message ?? "toast")}
+          {props.children}
+        </div>
+      </div>
+    </ToastContext.Provider>
   );
-}
+};
+
+const ToastContext = createContext<{
+  toast: ToastType;
+  setToast: React.Dispatch<React.SetStateAction<ToastType>>;
+}>({
+  toast: {},
+  setToast: () => {},
+});
 
 export default Toast;
