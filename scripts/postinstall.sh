bcfc7ce1fe68f6224fb634ed185830c39b6ab1a6
mimi99 <0mini9939@gmail.com>
2024-04-16 22:44:26 +0900

    v0.1.1-alpha.2

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,26 +3,15 @@ set -e
 
 # skip postinstall if npm install for development
 # rollup.config.js is not included in dist
-if [ -f "rollup.config.mjs" ]; then
-  echo "skipping your package's postinstall routine."
-  exit 0
-fi
+# if [ -f "rollup.config.mjs" ]; then
+#   echo "skipping your package's postinstall routine."
+#   exit 0
+# fi
 
 # copy files from dist folder into root project folder
-#
-# echo 'Copying files from dist folder into ${sub_directory} project folder...'
-# cp -r ${sub_directory}/dist/* ./${sub_directory} && rm -rf ${sub_directory}/dist
+echo 'Copying files from dist/types folder into root project folder...'
+cp -r dist/types/* ./ && rm -rf dist/types
 
-# copy files from dist folder into root project folder
-echo 'Copying files from dist folder into root project folder...'
-cp -r dist/* ./ && rm -rf dist
-
-# copy files from dist folder into atom project folder
-echo 'Copying files from dist folder into atom project folder...'
-cp -r atom/dist/* ./atom && rm -rf atom/dist
-
-# copy files from dist folder into core project folder
-echo 'Copying files from dist folder into core project folder...'
-cp -r core/dist/* ./core && rm -rf core/dist
+rm -rf dist
 
 echo 'Postinstall done!'
