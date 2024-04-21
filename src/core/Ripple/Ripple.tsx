9164f7ba158866720992173dc055879b33b7e13e
mimi99 <0mini9939@gmail.com>
2024-04-21 23:31:54 +0900

    feat: ripple에 터치 이벤트 추가

--- src/core/Ripple/Ripple.tsx
+++ src/core/Ripple/Ripple.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import "./Ripple.scss";
 
+import { isMouseEvent } from "util/event";
+
 export type RippleProps = Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>, keyof RipplePropsType> & RipplePropsType;
+
 type RipplePropsType = { parentRef: React.RefObject<HTMLElement>; isTouch?: boolean };
 
 const Ripple = ({ ...props }: RippleProps) => {
@@ -25,19 +28,18 @@ const Ripple = ({ ...props }: RippleProps) => {
       return;
     }
 
-    const appendAnimateEl = (event: MouseEvent) => {
+    const appendAnimateEl = (event: MouseEvent | TouchEvent) => {
       const ripple = document.createElement("div");
       const rect = rippleRootEl.getBoundingClientRect();
       ripple.className = "animate";
-
-      ripple.style.left = !isTouch ? `${event.clientX - rect.left}px` : `50%`;
-      ripple.style.top = !isTouch ? `${event.clientY - rect.top}px` : `50%`;
+      ripple.style.left = !isTouch && isMouseEvent(event) ? `${event.clientX - rect.left}px` : `50%`;
+      ripple.style.top = !isTouch && isMouseEvent(event) ? `${event.clientY - rect.top}px` : `50%`;
       ripple.style.setProperty("--material-scale", `${parentEl.offsetWidth}`);
 
       rippleRootEl.append(ripple);
     };
 
-    const removeAnimateEl = (_event: MouseEvent) => {
+    const removeAnimateEl = (_event: MouseEvent | TouchEvent) => {
       const currentTimer = setTimeout(() => {
         const animateEl = rippleRootEl.querySelector(".animate");
 
@@ -50,14 +52,20 @@ const Ripple = ({ ...props }: RippleProps) => {
       timer.push(currentTimer);
     };
 
+    parentEl.addEventListener("touchstart", appendAnimateEl);
     parentEl.addEventListener("mousedown", appendAnimateEl);
+
+    parentEl.addEventListener("touchend", removeAnimateEl);
     parentEl.addEventListener("mouseup", removeAnimateEl);
     parentEl.addEventListener("mouseleave", removeAnimateEl);
 
     return () => {
+      parentEl.removeEventListener("touchstart", appendAnimateEl);
       parentEl.removeEventListener("mousedown", appendAnimateEl);
+
       parentEl.removeEventListener("mouseup", removeAnimateEl);
       parentEl.removeEventListener("mouseleave", removeAnimateEl);
+      parentEl.removeEventListener("touchend", removeAnimateEl);
 
       timer.forEach(clearTimeout);
     };
