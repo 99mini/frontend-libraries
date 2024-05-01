954c84b6fc9384eb262c404ae0de4156e8105e4c
mimi99 <0mini9939@gmail.com>
2024-05-01 22:36:16 +0900

    chore: temp url

--- packages/atom/src/Button/Button.tsx
+++ packages/atom/src/Button/Button.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-import Ripple from "@core/Ripple";
+// import Ripple from "@core/Ripple";
 
 export type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> &
   React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
@@ -14,7 +14,7 @@ const Button = ({ href, ...props }: ButtonProps) => {
     const anchoreRef = useRef<HTMLAnchorElement>(null);
     return (
       <a href={href} {...props} className={classNames("Mini-Button Mini-Button-anchor", props.className)} ref={anchoreRef}>
-        <Ripple parentRef={anchoreRef} />
+        {/* <Ripple parentRef={anchoreRef} /> */}
         <span>{props.children ?? "Anchor Button"}</span>
       </a>
     );
@@ -23,7 +23,7 @@ const Button = ({ href, ...props }: ButtonProps) => {
   const buttonRef = useRef<HTMLButtonElement>(null);
   return (
     <button {...props} className={classNames("Mini-Button", props.className)} ref={buttonRef}>
-      <Ripple parentRef={buttonRef} />
+      {/* <Ripple parentRef={buttonRef} /> */}
       <span>{props.children ?? "Button"}</span>
     </button>
   );
