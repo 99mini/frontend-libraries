84ac903c963f1bc819622c463d2edfca5be8e079
mimi99 <0mini9939@gmail.com>
2024-05-13 23:26:18 +0900

    feat: tab animation

--- packages/molecular/src/Tab/Tab.stories.tsx
+++ packages/molecular/src/Tab/Tab.stories.tsx
@@ -3,28 +3,28 @@ import { userEvent, within } from "@storybook/test";
 
 import React from "react";
 
-import Tab, { TabProps, Tabs } from "./Tab";
+import Tab, { TabsProps, Tabs } from "./Tab";
 import classNames from "classnames/bind";
 import styles from "./Tab.scss";
 
 const meta = {
-  component: Tab,
+  component: Tabs,
   title: "molecular/Tab",
   tags: ["autodocs"],
-} satisfies Meta<typeof Tab>;
+} satisfies Meta<typeof Tabs>;
 
 export default meta;
 
-type Story = StoryObj<typeof Tab>;
+type Story = StoryObj<typeof Tabs>;
 
 const cx = classNames.bind(styles);
 
-const Template = (args: TabProps) => {
+const Template = (args: TabsProps) => {
   const handleClick1 = () => console.log("button 1");
   const handleClick2 = () => console.log("button 2");
   const handleClick3 = () => console.log("button 3");
   return (
-    <Tabs>
+    <Tabs {...args}>
       <Tab onClick={handleClick1}>button 1</Tab>
       <Tab onClick={handleClick2}>button 2</Tab>
       <Tab onClick={handleClick3}>button 3</Tab>
@@ -32,12 +32,12 @@ const Template = (args: TabProps) => {
   );
 };
 
-const DifferentSizesTemplate = (args: TabProps) => {
+const DifferentSizesTemplate = (args: TabsProps) => {
   const handleClick1 = () => console.log("button 1");
   const handleClick2 = () => console.log("short");
   const handleClick3 = () => console.log("long button size...!");
   return (
-    <Tabs>
+    <Tabs {...args}>
       <Tab onClick={handleClick1}>button 1</Tab>
       <Tab onClick={handleClick2}>short</Tab>
       <Tab onClick={handleClick3}>long button size...!</Tab>
@@ -56,6 +56,11 @@ export const Default: Story = {
     }
   },
   args: {},
+  render: Template,
+};
+
+export const NoneAnimation: Story = {
+  args: { animation: false },
   render: Template,
 };
 
--- packages/molecular/src/Tab/Tab.tsx
+++ packages/molecular/src/Tab/Tab.tsx
@@ -1,4 +1,11 @@
-import React, { Dispatch, createContext, useContext, useState } from "react";
+import React, {
+  Dispatch,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import classNames from "classnames";
 import { Button } from "@99mini/atom";
 
@@ -10,6 +17,12 @@ export type TabProps = React.DetailedHTMLProps<
 > &
   TabPropsType;
 
+export type TabsProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> &
+  TabsPropsType;
+
 type TabPropsType = {
   href?: string;
   onClick?: (
@@ -18,32 +31,78 @@ type TabPropsType = {
       | React.MouseEvent<HTMLAnchorElement>,
   ) => void;
 };
+type TabsPropsType = {
+  animation?: boolean;
+};
 
 let tabId = 0;
 
-const TabContext = createContext<{
+type TabContextType = {
   activeTab: number;
   setActiveTab: Dispatch<React.SetStateAction<number>>;
-}>({
-  activeTab: tabId,
-  setActiveTab: () => {},
-});
-
-const TabProvider = ({ children }: { children: React.ReactNode }) => {
-  const [activeTab, setActiveTab] = useState(tabId);
-
-  return (
-    <TabContext.Provider value={{ activeTab, setActiveTab }}>
-      {children}
-    </TabContext.Provider>
-  );
+  animation: boolean;
+  tabsLeft: number;
+  setTabWidth: Dispatch<React.SetStateAction<number>>;
+  setTabLeft: Dispatch<React.SetStateAction<number>>;
 };
 
-export const Tabs = ({ ...props }: TabProps) => {
+const TabContext = createContext<TabContextType>({
+  activeTab: tabId,
+  setActiveTab: () => {},
+  animation: true,
+  tabsLeft: 0,
+  setTabWidth: () => {},
+  setTabLeft: () => {},
+});
+
+export const Tabs = ({ animation = true, ...props }: TabsProps) => {
+  const { className, children, ...rest } = props;
+
+  const [activeTab, setActiveTab] = useState(tabId);
+  const [tabWidth, setTabWidth] = useState(0);
+  const [tabLeft, setTabLeft] = useState(0);
+
+  const tabsRef = useRef<HTMLDivElement>(null);
+  const [tabsLeft, setTabsLeft] = useState(0);
+
+  useEffect(() => {
+    if (tabsRef.current) {
+      setTabsLeft(tabsRef.current.getBoundingClientRect().left);
+    }
+  }, []);
+
   return (
-    <TabProvider>
-      <div className={classNames("Mini-Tabs")}>{props.children}</div>
-    </TabProvider>
+    <TabContext.Provider
+      value={{
+        activeTab,
+        setActiveTab,
+        tabsLeft,
+        animation,
+        setTabWidth,
+        setTabLeft,
+      }}
+    >
+      <div
+        {...rest}
+        className={classNames(
+          "Mini-Tabs",
+          className,
+          animation ? "animation" : "",
+        )}
+        ref={tabsRef}
+      >
+        {children}
+        {animation && (
+          <div
+            className={classNames("Mini-Tabs-Indicator")}
+            style={{
+              width: tabWidth,
+              left: tabLeft,
+            }}
+          />
+        )}
+      </div>
+    </TabContext.Provider>
   );
 };
 
@@ -55,25 +114,56 @@ export const Tab = ({ ...props }: TabProps) => {
     throw new Error("Tab must be used within a Tabs component");
   }
 
-  const [id] = useState(() => tabId++);
+  const {
+    activeTab,
+    setActiveTab,
+    animation,
+    tabsLeft,
+    setTabLeft,
+    setTabWidth,
+  } = context;
 
-  const isActive = context.activeTab === id;
+  const [id] = useState(() => tabId++);
+  const tabRef = useRef<HTMLDivElement>(null);
+
+  const isActive = activeTab === id;
 
   const handleClick = (
     event:
       | React.MouseEvent<HTMLButtonElement>
       | React.MouseEvent<HTMLAnchorElement>,
   ) => {
-    context.setActiveTab(id);
+    setActiveTab(id);
     onClick && onClick(event);
   };
 
+  useEffect(() => {
+    if (!animation || !isActive) {
+      return;
+    }
+
+    const tabEl = tabRef.current;
+
+    if (!tabEl) {
+      return;
+    }
+
+    const tabRect = tabEl.getBoundingClientRect();
+
+    setTabWidth(tabRect.width);
+    setTabLeft(tabRect.left - tabsLeft);
+  }, [animation, isActive]);
+
   return (
-    <div {...divProps} className={classNames("Mini-Tab", className)}>
+    <div
+      {...divProps}
+      className={classNames("Mini-Tab", isActive ? "active" : "", className)}
+      ref={tabRef}
+    >
       <Button
         href={href}
         onClick={handleClick}
-        className={classNames("Mini-Tab-Button", isActive ? "active" : "")}
+        className={classNames("Mini-Tab-Button")}
       >
         {props.children}
       </Button>
