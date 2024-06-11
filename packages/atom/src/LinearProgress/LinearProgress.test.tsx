035b726ec2e8ec2034f749a9028e12eec0b7c949
mimi99 <0mini9939@gmail.com>
2024-06-11 22:23:39 +0900

    feat: test coverage height props

--- packages/atom/src/LinearProgress/LinearProgress.test.tsx
+++ packages/atom/src/LinearProgress/LinearProgress.test.tsx
@@ -4,7 +4,15 @@ import LinearProgress from "./LinearProgress";
 
 describe("LinearProgress", () => {
   it("renders", () => {
-    const result = render(<LinearProgress />);
+    const result = render(
+      <LinearProgress data-testid="test-linear-progress" />,
+    );
+
+    const container = result.getByTestId("test-linear-progress");
+    const style = getComputedStyle(container);
+
+    expect(style.getPropertyValue("--linear-progress-height")).toBeFalsy();
+
     expect(result).toBeTruthy();
   });
 
@@ -18,5 +26,15 @@ describe("LinearProgress", () => {
     expect(
       container.querySelector(".YnI-LinearProgress-Indeterminate"),
     ).toBeTruthy();
+  });
+
+  it("renders with hegiht", () => {
+    const result = render(
+      <LinearProgress height={10} data-testid="test-linear-progress" />,
+    );
+    const container = result.getByTestId("test-linear-progress");
+
+    const style = getComputedStyle(container);
+    expect(style.getPropertyValue("--linear-progress-height")).toBe("10px");
   });
 });
